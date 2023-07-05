8337f5f089886d1045ba5324ad4db4d5a1fb337e
noonever <noonevernowhere@gmail.com>
2023-07-05 22:43:19 +0300

    paths

--- src/ip.py
+++ src/ip.py
@@ -1,7 +1,7 @@
 from geoip2 import database
 from geoip2.types import IPAddress
 
-reader = database.Reader('ip_db/GeoLite2-Country.mmdb')
+reader = database.Reader('../ip_db/GeoLite2-Country.mmdb')
 
 def get_country_iso(ip: IPAddress):
     return reader.country(ip).country.iso_code
